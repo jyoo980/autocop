0ef3dcb658df3a3cc9ea3c9f1718c44b798573c8
James Yoo <jyoo980@gmail.com>
2021-04-23 12:29:56 -0700

    adding automated issue creator
    Unfortunately, the GitHub API doesn't let you programmatically create
    issues on repositories that you don't own (kinda makes sense). This is
    annoying when I have to go over tens of repositories at the end of term
    and issue takedown notices. This should help automate the process.

---
+++ create-issues.py
@@ -0,0 +1,48 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+title_text = "ACTION REQUIRED: REPO VISIBILITY"
+body_text = "TODO: fill this in"
+
+def wait(secs):
+  time.sleep(secs)
+
+def create_issue(url):
+  driver.get(url)
+  # Fill in title
+  issue_title = driver.find_element_by_id("issue_title")
+  issue_title.send_keys(title_text)
+  wait(5)
+  # Fill in body
+  issue_body = driver.find_element_by_id("issue_body")
+  issue_body.send_keys(body_test)
+  wait(5)
+  # Submit
+  submit_button = driver.find_element_by_xpath(
+    '//button[normalize-space()="Submit new issue"]'
+  )
+  submit_button.click()
+  
+
+issue_urls = ["TODO: fill this in"]
+path_to_user_profile = "/Users/<username>/Library/Application Support/Google/Chrome/"
+driver = None
+
+try:
+  opts = webdriver.ChromeOptions()
+  opts.add_argument(f"--user-data-dir={path_to_user_profile}")
+  driver = webdriver.Chrome(options=opts)
+
+  # Pre-process urls
+  issue_urls = [url + "/new" for url in issue_urls]
+
+  for url in issue_urls:
+    create_issue(url)
+     # Don't go too fast
+    wait(5)
+
+  driver.quit()
+except:
+  if driver:
+    driver.quit()
